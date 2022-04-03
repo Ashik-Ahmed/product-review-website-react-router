0d80055f4108c4a0c3cefa3662feffae842a2533
Ashik-Ahmed <ashikahmed121@gmail.com>
2022-04-04 01:51:51 +0600

    product description and order now button added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,9 +7,11 @@ const Home = () => {
     return (
         <div>
             <div className='md:flex justify-between items-center md:mx-28'>
-                <div className=''>
+                <div className='w-3/4 px-16'>
                     <h1 className='text-6xl'>A BRIEF HISTORY OF TIME</h1>
                     <h3 className='text-3xl text-red-400 text-right mb-8'>-STEPHEN HAWKING</h3>
+                    <p className='italic text-blue-600'>A landmark volume in science writing by one of the great minds of our time, Stephen Hawking’s book explores such profound questions as: How did the universe begin—and what made its start possible? Does time always flow forward? Is the universe unending—or are there boundaries? Are there other dimensions in space? What will happen when it all ends?</p>
+                    <button className='text-lg font-semibold bg-transparent hover:bg-orange-400 border-2 border-blue-400 py-2 px-4 rounded-3xl mt-8'>Order Now</button>
                 </div>
                 <div className=''>
                     <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1333578746l/3869.jpg" alt="" />
